ce3211c4e4f1c3c61e02c2deec15aba1c3920d91
sherin200 <sherinsebastian21>
2022-11-30 12:58:31 +0530

    gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeNestComponent } from './home-nest/home-nest.component';
+import { GalleryNestComponent } from './gallery-nest/gallery-nest.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeNestComponent
+    HomeNestComponent,
+    GalleryNestComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/gallery-nest/gallery-nest.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery-nest',
+  templateUrl: './gallery-nest.component.html',
+  styleUrls: ['./gallery-nest.component.css']
+})
+export class GalleryNestComponent {
+
+}
