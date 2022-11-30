2029ca887437bfdb4fb241576d9eb98f4961ce3b
sherin200 <sherinsebastian21>
2022-11-30 17:52:05 +0530

    add navbar

---
+++ src/app/navbar-add/navbar-add.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-add',
+  templateUrl: './navbar-add.component.html',
+  styleUrls: ['./navbar-add.component.css']
+})
+export class NavbarAddComponent {
+
+}
